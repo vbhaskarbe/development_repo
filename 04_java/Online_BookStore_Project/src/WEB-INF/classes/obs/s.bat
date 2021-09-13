a24abbfa307f58fb261723656dc9958d4dacedcd
bhasvara <bhasvara@cisco.com>
2021-09-13 19:56:35 +0530

    Removed wanted bad code

--- 04_java/Online_BookStore_Project/src/WEB-INF/classes/obs/s.bat
+++
@@ -1,3 +0,0 @@
-path c:\jdk1.4\bin;c:\windows\system32
-set classpath=.;c:\jdk1.4\lib\tools.jar;c:\tomcat\common\lib\servlet.jar;c:\tomcat\common\lib\xerces.jar;d:\tomcat\common\lib\activation.jar;c:\tomcat\common\lib\mail.jar;c:\tomcat\webapps\obs\web-inf\classes;c:\tomcat\webapps\obs\web-inf\lib\order_client.jar;c:\weblogic\weblogic700\server\lib\weblogic.jar
-
