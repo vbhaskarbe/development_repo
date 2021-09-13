28b2882c736df3b73b78859df4ce401db861ad23
bhasvara <bhasvara@cisco.com>
2021-09-13 19:55:19 +0530

    Removed dummy code

--- 04_java/Online_BookStore_Project/src/order/Se.bat
+++
@@ -1,4 +0,0 @@
-set classpath=.;c:\jdk1.4\lib\tools.jar;c:\weblogic\weblogic700\server\lib\weblogic.jar;d:\weblogic7\weblogic700
-
-path=c:\weblogic\weblogic700\server\bin;c:\jdk1.4\bin;c:\windows\system32
-
