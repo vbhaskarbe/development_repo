6ea9bda2bbe1e10fa0488937d6ff3d9a951048eb
bhasvara <bhasvara@cisco.com>
2021-09-24 13:33:09 +0530

    Fixed formatting issue

--- 01_shell/03_case_esac.sh
+++ 01_shell/03_case_esac.sh
@@ -9,15 +9,15 @@
 echo "Enter a tool (jenkins, jira, git, sonarqube): "
 read TOOL
 case "$TOOL" in
-   "jenkins") 	echo "jenkins is a CI/CD tool."
+   "jenkins") echo "jenkins is a CI/CD tool."
    ;;
-   "jira") 		echo "jira is a bug reporting and agile tool." 
+   "jira") echo "jira is a bug reporting and agile tool." 
    ;;
-   "git") 		echo "git is one of source-control management tool." 
+   "git") echo "git is one of source-control management tool." 
    ;;
    "sonarqube") echo “sonarqube is code coverage and static analysis tool”
    ;;
-	*)	 	    echo "invalid tool $TOOL. valid inputs are: jenkins, git, jira, sonarqube."
+   *) echo "invalid tool $TOOL. valid inputs are: jenkins, git, jira, sonarqube."
    ;;
 esac
 
