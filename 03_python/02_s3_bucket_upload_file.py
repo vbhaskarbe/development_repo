9442c4b57c8581562a2b9683b395593d889c4609
bhasvara <bhasvara@cisco.com>
2021-09-26 09:03:23 +0530

    Fixed typo

--- 03_python/02_s3_bucket_upload_file.py
+++ 03_python/02_s3_bucket_upload_file.py
@@ -17,25 +17,25 @@ export S3_BUCKET_NAME='<your_s3_bucket_name>'
 """
 
 def upload_to_aws_s3(local_file, bucket, s3_file):
-    """Upload a file to an S3 bucket
-    :param local_file: File to upload
-    :param bucket    : Bucket to upload to
-    :param s3_file   : S3 object name or default file_name
+    """ Upload a file to an S3 bucket
+    :param local_file : File to upload
+    :param bucket     : Bucket to upload to
+    :param s3_file    : file name in S3
     :return: True if file was uploaded, else False
     """
     s3_client = boto3.client('s3',
         region_name='ap-south-1',
     )
     try:
-        print("Uploading file: ", local_file)
+        print("INFO: Uploading file: ", local_file)
         response = s3_client.upload_file(local_file, bucket, s3_file, ExtraArgs = {'ACL': 'public-read'})
-        print("Upload Successful")
+        print("INFO: File upload was Successful")
         return True
     except FileNotFoundError:
-        print("The file was not found")
+        print("ERROR: The file was not found")
         return False
     except NoCredentialsError:
-        print("Credentials not available")
+        print("ERROR: Credentials not available")
         return False
 
 s3_local_upload_file = os.environ.get('S3_UPLOAD_LOCALFILE')
