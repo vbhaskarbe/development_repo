bec6c577296a319dba6d8bb6b033b021df61e944
bhasvara <bhasvara@cisco.com>
2021-09-27 17:35:18 +0530

    Added testcases for testing 'case..esac' program

--- 01_shell/03_case_esac.sh
+++ 01_shell/03_case_esac.sh
@@ -3,11 +3,12 @@
 #
 # A Shell program to check demonstrate use of case..esac construct
 #
-
+# Execute is as : bash 03_case_esac.sh <option> 
 #TOOL="jenkins"
 
-echo "Enter a tool (jenkins, jira, git, sonarqube): "
-read TOOL
+#echo "Enter a tool (jenkins, jira, git, sonarqube): "
+#read TOOL
+TOOL=$1
 case "$TOOL" in
    "jenkins") echo "jenkins is a CI/CD tool."
    ;;
@@ -15,7 +16,7 @@ case "$TOOL" in
    ;;
    "git") echo "git is one of source-control management tool." 
    ;;
-   "sonarqube") echo “sonarqube is code coverage and static analysis tool”
+   "sonarqube") echo "sonarqube is code coverage and static analysis tool"
    ;;
    "*") echo "invalid tool $TOOL. valid inputs are: jenkins, git, jira, sonarqube."
    ;;
